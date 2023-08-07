60e88a2e39a90af6d862af5fb33a7ff11e458473
Waiee Zainol <69743222+waiee@users.noreply.github.com>
2023-08-07 21:43:10 +0800

    get html

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ import requests
 #get html text
 html_text =  requests.get('https://www.futurepedia.io').text
 soup = BeautifulSoup(html_text, 'lxml')
-jobs = soup.find_all('div', class_ = 'MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-sm-6 MuiGrid-grid-md-4 MuiGrid-grid-lg-4 css-12y6uts')
-print(jobs)
+ai = soup.find('div', class_ = 'MuiBox-root css-a0wou3')
+ai_name = ai.find('h3', class_='MuiTypography-root MuiTypography-h6 MuiTypography-alignLeft css-nxqa8p')
+print(ai_name)
