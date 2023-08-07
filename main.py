d295c179b76a9e6e48fa4db9c0bc1043f06c42c6
Waiee Zainol <69743222+waiee@users.noreply.github.com>
2023-08-07 21:27:38 +0800

    html

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from bs4 import BeautifulSoup 
 import requests
 
-#get website link
-html_text =  requests.get('https://www.futurepedia.io')
+#get html text
+html_text =  requests.get('https://www.futurepedia.io').text
 print(html_text)
  
 
