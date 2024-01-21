89687096a048b95ede9185901ba6a16265272a9c
Waiee Zainol <69743222+waiee@users.noreply.github.com>
2024-01-21 19:09:07 +0800

    c

--- main.py
+++ main.py
@@ -3,12 +3,9 @@ import requests
 
 #get html text
 html_text =  requests.get('https://www.futurepedia.io').text
-
 #define soup
 soup = BeautifulSoup(html_text, 'lxml')
-
-#find all by tag
+#find all by cards
 cards = soup.find_all('div', class_="flex flex-col items-start")
-
 for card in cards:
     print("AI Tools Name: ", card.p.text)
