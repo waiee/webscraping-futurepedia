31ac9652acec8dd00f35ff5755693c22acee660e
Waiee Zainol <69743222+waiee@users.noreply.github.com>
2024-01-21 19:25:11 +0800

    link

--- main.py
+++ main.py
@@ -9,16 +9,17 @@ soup = BeautifulSoup(html_text, 'lxml')
 cards = soup.find_all('div', class_="flex flex-col items-start")
 links = soup.find_all('div', class_ ="px-6 mt-auto flex items-center justify-between pb-4")
 
+#find name
 for card in cards:
     ai_name = card.p.text
-
-    #find name
     print("AI Tools Name: ", ai_name)
 
-    #find link
+#find button link
 for link in links:
-    button_link = card.span.text
+    button_link = link.span.text
+    ai_link = link.a.href
+
     if button_link == "Get Deal":
-            print("Get Deal: ", button_link)
+            print("Get Deal: ", ai_link)
     else:
-        print("Visit Website: ", button_link)
+        print("Visit Website: ", ai_link)
