71b40486e9bd5a8c8415bc08a19b56e65bdd1139
Waiee Zainol <69743222+waiee@users.noreply.github.com>
2024-01-21 18:53:37 +0800

    c

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ html_text =  requests.get('https://www.futurepedia.io').text
 
 #define soup
 soup = BeautifulSoup(html_text, 'lxml')
-# ai = soup.find('div', class_ = 'MuiBox-root css-a0wou3')
-# ai_name = ai.find('h3', class_='MuiTypography-root MuiTypography-h6 MuiTypography-alignLeft css-nxqa8p')
-# print(ai_name.prettify())
 
-##test test
+#find all by tag
+tags = soup.find_all('h5')
+print(tags)
