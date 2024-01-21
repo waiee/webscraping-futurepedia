d8a128a8d23d3cc1a6f3fa22be20bc80c00be080
Waiee Zainol <69743222+waiee@users.noreply.github.com>
2024-01-21 19:32:04 +0800

    c

--- main.py
+++ main.py
@@ -11,18 +11,22 @@ cards = soup.find_all('div', class_='flex flex-col items-start')
 links = soup.find_all('div', class_='px-6 mt-auto flex items-center justify-between pb-4')
 
 #find all desc
+descs = soup.find_all('div', class_="px-6")
 
 # Extract and print AI tools name and link
-for card, link in zip(cards, links):
+for card, link, desc in zip(cards, links, descs):
     ai_name = card.p.text
     button_link = link.span.text.strip()
     ai_link = link.find('a')['href']
+    ai_desc = desc.p.text
 
     if button_link == "Get Deal":
         print("AI Tools Name: ", ai_name)
         print("Get Deal: ", ai_link)
+        print("Description: ", ai_desc )
     else:
         print("AI Tools Name: ", ai_name)
         print("Visit Website: ", ai_link)
+        print("Description: ", ai_desc )
     print(" ")
 
